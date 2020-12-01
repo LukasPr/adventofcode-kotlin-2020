b3eb31881aaecba3af6de6575f92c3444792fb6a
Lukas Probst <lukas.probst@gmx.de>
2020-12-01 22:07:08 +0100

    first commit

---
+++ src/main/kotlin/bmw/aoc/Day1.kt
@@ -0,0 +1,27 @@
+package bmw.aoc
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    println("Solution Part 1: " + chooseNumbersTwoEntries(readInputFile("src/main/resources/day1-input")))
+    println("Solution Part 2: " + chooseNumbersThreeEntries(readInputFile("src/main/resources/day1-input")))
+}
+
+fun chooseNumbersTwoEntries(listOfAllNumbers: List<String>): Int {
+    val inputList = listOfAllNumbers.map { it.toInt() }
+    return inputList
+        .flatMap { i
+            -> inputList.map { j -> Pair(i, j) } }
+        .first { it.first + it.second == 2020 }
+        .let { it.first * it.second }
+}
+
+fun chooseNumbersThreeEntries(listOfAllNumbers: List<String>): Int {
+    val inputList = listOfAllNumbers.map { it.toInt() }
+    return inputList
+        .flatMap { i -> inputList.flatMap { j -> inputList.map { k -> Triple(i, j, k) } } }
+        .first { it.first + it.second + it.third == 2020 }
+        .let { it.first * it.second * it.third}
+}
+
+fun readInputFile(fileName: String): List<String> = File(fileName).readLines()
---
+++ src/test/kotlin/bmw/aoc/HelloTest.kt
@@ -0,0 +1,8 @@
+package bmw.aoc
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
