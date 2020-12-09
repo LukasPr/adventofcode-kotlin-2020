0c72c9fe413abc108316b78a792ff21d055c2d70
Lukas Probst (FG-7-L-24) <Lukas.Probst@bmw.de>
2020-12-09 08:49:02 +0100

    quickly finished day8 in a dirty way

--- src/main/kotlin/bmw/aoc/Day8.kt
+++ src/main/kotlin/bmw/aoc/Day8.kt
@@ -2,14 +2,16 @@ package bmw.aoc
 
 fun main() {
     println(readInputFile("src/main/resources/day8-input"))
-    println(determineValueOfAccumulator(readInputFile("src/main/resources/day8-input")))
+    println(determineValueOfAccumulator())
+    println(swapOneOperation())
 }
 
-val test = readInputFile("src/main/resources/day8-input").map { it.split(" ") }
-val instructions = test.map { Pair<String, String>(it.first(), it[it.lastIndex]) }
+val instructions = readInputFile("src/main/resources/day8-input")
+    .map { it.split(" ") }
+    .map { Pair<String, String>(it.first(), it[it.lastIndex]) }
 
-
-fun determineValueOfAccumulator(input: List<String>): Int {
+//part1
+fun determineValueOfAccumulator(): Int {
     var (position, accumulator, alreadyVisited) = Triple(0, 0, mutableListOf<Int>())
     while (position != instructions.lastIndex) {
         if (position in alreadyVisited) return accumulator
@@ -28,15 +30,34 @@ fun determineValueOfAccumulator(input: List<String>): Int {
     return accumulator
 }
 
-//unfinished....
-fun swapOneOperation(input: List<String>): Int {
+//part2
+fun swapOneOperation(): Int {
+    val copyOfInstructions = instructions.toMutableList()
+    var result = 0
+    instructions.forEachIndexed { index, pair ->
+        when (pair.first) {
+            "jmp" -> copyOfInstructions[index] = copyOfInstructions[index].copy(first = "nop")
+            "nop" -> copyOfInstructions[index] = copyOfInstructions[index].copy(first = "jmp")
+            else -> 0
+        }
+        if (determineValueOfAccumulatorForPartTwo(copyOfInstructions) != -1) {
+            result = determineValueOfAccumulatorForPartTwo(copyOfInstructions)
+            return result
+        } else {
+            pair.copy(first = pair.first)
+            copyOfInstructions[index] = copyOfInstructions[index].copy(first = pair.first)
+        }
+    }
+    return 0
+}
+
+//part2
+fun determineValueOfAccumulatorForPartTwo(input: List<Pair<String, String>>): Int {
     var (position, accumulator, alreadyVisited) = Triple(0, 0, mutableListOf<Int>())
-    var count = 0
-    while (position != instructions.lastIndex) {
-        count++
-//        if (position in alreadyVisited) return accumulator
-        val currentInstruction = instructions[position]
-//        alreadyVisited.add(position)
+    while (position != input.lastIndex) {
+        if (position in alreadyVisited) return -1
+        val currentInstruction = input[position]
+        alreadyVisited.add(position)
         when (currentInstruction.first) {
             "acc" -> {
                 accumulator += currentInstruction.second.toInt()
@@ -49,7 +70,6 @@ fun swapOneOperation(input: List<String>): Int {
     }
     return accumulator
 }
-
 
 
 
